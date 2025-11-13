d549113ec50ae9e4b0a20a001fdc5a0c45da0ead
vinaytz <xvinaytiwari@gmail.com>
2025-11-14 01:17:29 +0530

    changes++

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-import express from 'express';
-const app = express();
+// import express from 'express';
+// const app = express();
 
-app.listen(3333, () => {
-  console.log("listening...");
-});
+// app.listen(3333, () => {
+//   console.log("listening...");
+// });
 
 
-app.get("/api", (req, res)=>{
-    console.log("Hello")
+// app.get("/api", (req, res)=>{
+//     console.log("Hello")
+//     setInterval(async () => {
+//     try {
+//       await fetch("https://skfood.onrender.com/");
+//       console.log("Response from 6363");
+//     } catch (err) {
+//       console.error("Error calling API:", err.message);
+//     }
+//     }, 850000);
+// })
+
+
+let intervalStarted = false;
+
+app.get("/", (req, res) => {
+  console.log("Hello");
+  res.send("Server is running!");
+
+  if (!intervalStarted) {
+    intervalStarted = true;
+
     setInterval(async () => {
-    try {
-      await fetch("http://localhost:6363/api/checkSeverRunning");
-      console.log("Response from 6363");
-    } catch (err) {
-      console.error("Error calling API:", err.message);
-    }
-    }, 850000);
-})
+      try {
+        const response = await fetch("https://skfood.onrender.com/");
+        const data = await response.text();
+        console.log("Response from 6363:", data);
+      } catch (err) {
+        console.error("Error calling API:", err.message);
+      }
+    }, 1000);
+  }
+});
